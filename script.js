325582c2af7e29fe2cb7b3a8204f2907253e52f6
Nemesis Lugo <yonaibereduardolugosaa@gmail.com>
2024-03-15 17:08:27 -0400

    ya casi

---
+++ script.js
@@ -0,0 +1,22 @@
+const lista = [
+"Pagina" ,
+"Hecha" ,
+"Con" ,
+"Amor" ,
+
+]
+const usuario = {
+    name: "leonardo",
+    age: "20",
+    password: "123",
+    job: "frontend develop",
+
+}
+
+undefined
+
+null
+console.log(usuario.job)
+if (10) {
+    console.log ("Se cumplio")
+}
